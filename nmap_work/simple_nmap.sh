3b0c4bc1a13f08144ce96876017e0f427415173d
shankin danila <shankindanila0@yandex.ru>
2023-02-05 17:46:01 +0300

    way to take config file from remote server

--- nmap_work/simple_nmap.sh
+++ nmap_work/simple_nmap.sh
@@ -4,5 +4,5 @@ nmap 192.168.57.10 -p21 --script ftp-anon >> output.txt
 nmap 192.168.57.10 -p21 --script ftp-bounce >> output.txt
 nmap 192.168.57.10 -p21 --script ftp-libopie >> output.txt
 nmap 192.168.57.10 -p21 --script ftp-syst >> output.txt
-#grep -E "tcp.*open" output.txt | awk '{ print $1$3 }' | sort | uniq >> simple_nmap.txt
-#rm output.txt simple_nmap.txt
+
+ansible-playbook ../playbooks/playbook_ftp_preparation.yml
